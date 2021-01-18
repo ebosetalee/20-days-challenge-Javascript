18d69391bbd5c56cf3d7367d267e2c11103e735b
ebosetalee <princess01ella.miley@gmail.com>
2021-01-18 14:09:12 +0100

    Initial commit

---
+++ wordsInReverse/words-in-reverse.js
@@ -0,0 +1,17 @@
+const randomWords = ["Shine bright like a diamond in the sky", 
+                    "fill the earth with your beautiful heart", 
+                    "make your happiness glow always"];
+const random = Math.floor(Math.random() * randomWords.length)+ 1;
+
+function wordInReverse(sentence) {
+    let reversed = sentence.split("").reverse().join("");
+    let words = [];
+    words = sentence.match(/\S+/g);
+    let wordsReversed = "";
+    for (let i = 0; i < words.length; i++) {
+        wordsReversed += words[i].split("").reverse().join("") + " "
+    };
+    return reversed + "\n \tAND \n" + wordsReversed
+};
+
+console.log(wordInReverse(randomWords[random]))
