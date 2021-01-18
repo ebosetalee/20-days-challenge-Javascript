af98b31a84ac85322ba1210d6bd211fa60e6bfc6
Raymond Tukpe <rtukpe@gmail.com>
2021-01-18 14:22:23 +0100

    chore: add .prettierrc file

--- wordsInReverse/words-in-reverse.js
+++ wordsInReverse/words-in-reverse.js
@@ -1,7 +1,9 @@
-const randomWords = ["Shine bright like a diamond in the sky", 
-                    "fill the earth with your beautiful heart", 
-                    "make your happiness glow always"];
-const random = Math.floor(Math.random() * randomWords.length)+ 1;
+const randomWords = [
+    "Shine bright like a diamond in the sky",
+    "fill the earth with your beautiful heart",
+    "make your happiness glow always"
+];
+const random = Math.floor(Math.random() * randomWords.length) + 1;
 
 function wordInReverse(sentence) {
     let reversed = sentence.split("").reverse().join("");
@@ -9,9 +11,9 @@ function wordInReverse(sentence) {
     words = sentence.match(/\S+/g);
     let wordsReversed = "";
     for (let i = 0; i < words.length; i++) {
-        wordsReversed += words[i].split("").reverse().join("") + " "
-    };
-    return reversed + "\n \tAND \n" + wordsReversed
-};
+        wordsReversed += words[i].split("").reverse().join("") + " ";
+    }
+    return reversed + "\n \tAND \n" + wordsReversed;
+}
 
-console.log(wordInReverse(randomWords[random]))
+console.log(wordInReverse(randomWords[random]));
